ed474793ee3ec3c4e2c3069bc42df05f459a7c8d
taijusugahara <96ayutuki@gmail.com>
2021-04-28 14:09:27 +0900

    画像選択

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -12,6 +12,7 @@ $(function () {
 //選択ボタンデータを配列に入れてカウントする関数
     var countA;
     var countB;
+    var countC;
     var box =[];
   $(".btn").each(function(){
     $(this).on('click',function(){
@@ -24,17 +25,35 @@ $(function () {
       countB = box.filter(function(y){
                       return y === "b"
                   }).length;
+      countC = box.filter(function(z){
+                      return z === "c"
+                  }).length;
     });
   });
 
 
+
 //結果を出力する関数
   $('.end').on('click',function(){
-    if( countA > countB ) {
+    //endクラスをクリックしたときの関数
+    if( countA > countB & countC) {
       $('#answer_01').css("display",""); //回答1
-    } else {
-      $('#answer_02').css("display","");//回答2
+    //answer_01のdisplayを""へ
+  } else if(countB > countA & countC){
+    $('#answer_02').css("display","");//回答2
+      //answer_02のdisplayを""へ
+  } else if(countC > countA & countB){
+    $('#answer_03').css("display","");//回答2
+  //answer_03のdisplayを""へ
+  } else if( countA == countB && countA > countC){
+    $('#answer_04').css("display","");//回答2
+  //answer_04のdisplayを""へ   
+  } else if( countA == countC && countA > countB){
+    $('#answer_05').css("display","");//回答2
+  //answer_05のdisplayを""へ
+  } else if( countB == countC && countB > countA){
+    $('#answer_06').css("display","");//回答2
+  //answer_06のdisplayを""へ
   }
-});
-
+  });
 });
