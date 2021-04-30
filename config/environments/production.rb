15cae7f343e76142a464c36db4cb603f3cb6e60e
taijusugahara <96ayutuki@gmail.com>
2021-04-30 18:54:46 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,8 +115,8 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { host: 'https://travel-app-35185.herokuapp.com' }
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-      user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_PASSWORD'],
+      user_name: 'apikey',
+      password: ENV['SENDGRID_API_KEY'],
       domain: "heroku.com",
       address: "smtp.SendGrid.net",
       port: 587,
